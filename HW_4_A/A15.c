40ed51b573539f1644923893a2c278db8c715be8
Бурый Павел Юрьевич <p.buriy@rdp.ru>
2025-03-05 08:00:21 +0300

    add HW

---
+++ HW_4_A/A15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float x1, y1;
+    float x2, y2;
+    float k, b;
+
+    printf("A(x1; y1): ");
+    scanf("%f%f", &x1, &y1);
+    printf("A(x2; y2): ");
+    scanf("%f%f", &x2, &y2);
+
+    k = (y1 - y2) / (x1 - x2);
+    b = y2 - k * x2;
+
+    printf("y = %.2fx + %.2f\n", k, b);
+}
